d11f237baf2be7bfad215e1e7cd9246767f031bc
Piotr Ostrowski <piotrostr@google.com>
2022-08-23 12:39:31 +0200

    feat: add example api to test for cuda

---
+++ api/gpu/app/main.py
@@ -0,0 +1,20 @@
+import torch
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def get_cuda_info() -> dict or str:
+    if not torch.cuda.is_available():
+        return "CUDA is not available"
+    return {
+        "device_name": torch.cuda.get_device_name(),
+        "device_count": torch.cuda.device_count(),
+        "device_properties": torch.cuda.get_device_properties(0),
+    }
+
+
+@app.get("/")
+def home():
+    return get_cuda_info()
