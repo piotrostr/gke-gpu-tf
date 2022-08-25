f864cf88725ea970f18591300b3258507daad08f
Piotr Ostrowski <piotrostr@google.com>
2022-08-25 17:23:16 +0200

    fix: resolve api json serialization err

--- api/gpu/app/main.py
+++ api/gpu/app/main.py
@@ -9,9 +9,9 @@ def get_cuda_info() -> dict or str:
     if not torch.cuda.is_available():
         return "CUDA is not available"
     return {
+        "visible_device": torch.cuda.get_device_name(0),
         "device_name": torch.cuda.get_device_name(),
         "device_count": torch.cuda.device_count(),
-        "device_properties": torch.cuda.get_device_properties(0),
     }
 
 
