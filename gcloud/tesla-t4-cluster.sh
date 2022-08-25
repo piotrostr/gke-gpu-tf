7dc7079636d4832ed5a9a616e0bc6801179ef283
Piotr Ostrowski <piotrostr@google.com>
2022-08-25 15:19:42 +0200

    fix: change the gpu consumption request to 1 to match the cluster capability

--- gcloud/tesla-t4-cluster.sh
+++ gcloud/tesla-t4-cluster.sh
@@ -2,6 +2,7 @@
 
 gcloud container clusters create tesla-t4-cluster \
   --accelerator type=nvidia-tesla-t4,count=1 \
+  --autoprovisioning-scopes=https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring,https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/compute \
   --zone us-central1-a \
   --machine-type n1-standard-4
 
@@ -12,4 +13,3 @@ kubectl apply \
   -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/cos/daemonset-preloaded-latest.yaml
 
 kubectl apply -f gpu-consuming-pod.yaml
-
